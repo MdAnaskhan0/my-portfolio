9b3e13c72bb9e7e68c71d3b783eefd468f866bbc
MdAnaskhan0 <mdanaskhan.201902037@gmail.com>
2025-07-14 13:54:33 +0600

    About me update

--- admin/src/pages/portfolio/AboutMe.jsx
+++ admin/src/pages/portfolio/AboutMe.jsx
@@ -8,6 +8,10 @@ const AboutMe = () => {
                     <div className="md:flex text-white">
                         <p className='text-lg text-gray-400 text-justify'>I am a Full Stack Developer with comprehensive expertise in HTML, CSS, JavaScript, React.js, and Tailwind CSS, along with a solid understanding of Express.js and MongoDB. This combination enables me to effectively create and manage reliable web applications across the entire development stack. I specialize in utilizing modern frameworks and technologies to deliver high-quality, industry-standard code that meets both user needs and business objectives. Driven by a strong passion for solving complex problems, I am committed to continuous learning and professional development.</p>
                     </div>
+
+                    <div className="md:flex text-white mt-4">
+                        <h3 className='text-white font-bold text-md sm:text-3xl capitalize'>What i'm doing</h3>
+                    </div>
                 </div>
             </div>
         </>
